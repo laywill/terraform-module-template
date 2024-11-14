9c533fa4bb3cd42871e58d7aa1198e28e9096a97
William Lay <laywill@gmail.com>
2024-11-14 23:41:54 +0000

    Fix and fix linting

--- test/example_test.go
+++ test/example_test.go
@@ -1,37 +1,59 @@
 package test
 
 import (
-    "testing"
-    "github.com/gruntwork-io/terratest/modules/terraform"
-    "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestTerraformVpc(t *testing.T) {
-    t.Parallel()
+// An example of how to test the simple Terraform module in examples/terraform-basic-example using Terratest.
+func TestTerraformBasicExample(t *testing.T) {
+	t.Parallel()
 
-    // Configure the Terraform options
-    terraformOptions := &terraform.Options{
-        // The path to where your Terraform code is located
-        TerraformDir: "../examples/example-name",
+	expectedText := "test"
+	expectedList := []string{expectedText}
+	expectedMap := map[string]string{"expected": expectedText}
 
-        // Variables to pass to our Terraform code using -var options
-        Vars: map[string]interface{}{
-            "example_variable": "example_value",
-        },
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		// website::tag::1::Set the path to the Terraform code that will be tested.
+		// The path to where our Terraform code is located
+		TerraformDir: "../examples/example-name",
 
-        // Disable colors in Terraform commands so its easier to parse stdout/stderr
-        NoColor: true,
-    }
+		// Variables to pass to our Terraform code using -var options
+		Vars: map[string]interface{}{
+			"example1": expectedText,
 
-    // Clean up resources with "terraform destroy" at the end of the test
-    defer terraform.Destroy(t, terraformOptions)
+			// We also can see how lists and maps translate between terratest and terraform.
+			"example_list": expectedList,
+			"example_map":  expectedMap,
+		},
 
-    // Run "terraform init" and "terraform apply". Fail the test if there are any errors.
-    terraform.InitAndApply(t, terraformOptions)
+		// Variables to pass to our Terraform code using -var-file options
+		VarFiles: []string{"varfile.tfvars"},
 
-    // Run `terraform output` to get the value of an output variable
-    placeholderNullID := terraform.Output(t, terraformOptions, "placeholder_null_id")
+		// Disable colors in Terraform commands so its easier to parse stdout/stderr
+		NoColor: true,
+	})
 
-    // Verify we're getting back the outputs we expect
-    assert.NotNil(t, placeholderNullID)
+	// website::tag::4::Clean up resources with "terraform destroy". Using "defer" runs the command at the end of the test, whether the test succeeds or fails.
+	// At the end of the test, run `terraform destroy` to clean up any resources that were created
+	defer terraform.Destroy(t, terraformOptions)
+
+	// website::tag::2::Run "terraform init" and "terraform apply".
+	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
+	terraform.InitAndApply(t, terraformOptions)
+
+	// Run `terraform output` to get the values of output variables
+	actualTextExample := terraform.Output(t, terraformOptions, "example1")
+	actualTextExample2 := terraform.Output(t, terraformOptions, "example2")
+	actualExampleList := terraform.OutputList(t, terraformOptions, "example_list")
+	actualExampleMap := terraform.OutputMap(t, terraformOptions, "example_map")
+
+	// website::tag::3::Check the output against expected values.
+	// Verify we're getting back the outputs we expect
+	assert.Equal(t, expectedText, actualTextExample)
+	assert.Equal(t, expectedText, actualTextExample2)
+	assert.Equal(t, expectedList, actualExampleList)
+	assert.Equal(t, expectedMap, actualExampleMap)
 }
